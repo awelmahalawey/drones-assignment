13063be5be9f7b1877452805976325ead5893b88
Ahmed Elmahalawey <a.elmahalawey@embermed.com>
2022-10-14 19:51:54 +0200

    - Adding 10 drones to the fleet

--- src/main/resources/db/migration/V1.1.5__insert_drones.sql
+++ src/main/resources/db/migration/V1.1.5__insert_drones.sql
@@ -10,119 +10,14 @@ INTO
   drone
   (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
 VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'CRUISER_WEIGHT', 'IDLE', 300, 30, 0, NOW(), NOW(), TRUE);
+  (RANDOM_UUID(), RANDOM_UUID(), 'CRUISER_WEIGHT', 'IDLE', 300, 100, 0, NOW(), NOW(), TRUE);
 
 INSERT
 INTO
   drone
   (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
 VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'MIDDLE_WEIGHT', 'IDLE', 100, 75, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 20, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 20, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 20, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 20, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 20, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 20, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 35, 0, NOW(), NOW(), TRUE);
-
-INSERT
-INTO
-  drone
-  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
-VALUES
-  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 20, 0, NOW(), NOW(), TRUE);
+  (RANDOM_UUID(), RANDOM_UUID(), 'MIDDLE_WEIGHT', 'IDLE', 100, 100, 0, NOW(), NOW(), TRUE);
 
 INSERT
 INTO
@@ -130,3 +25,45 @@ INTO
   (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
 VALUES
   (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 100, 0, NOW(), NOW(), TRUE);
+
+INSERT
+INTO
+  drone
+  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
+VALUES
+  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 100, 0, NOW(), NOW(), TRUE);
+
+INSERT
+INTO
+  drone
+  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
+VALUES
+  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 100, 0, NOW(), NOW(), TRUE);
+
+INSERT
+INTO
+  drone
+  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
+VALUES
+  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 100, 0, NOW(), NOW(), TRUE);
+
+INSERT
+INTO
+  drone
+  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
+VALUES
+  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 100, 0, NOW(), NOW(), TRUE);
+
+INSERT
+INTO
+  drone
+  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
+VALUES
+  (RANDOM_UUID(), RANDOM_UUID(), 'LIGHT_WEIGHT', 'IDLE', 100, 100, 0, NOW(), NOW(), TRUE);
+
+INSERT
+INTO
+  drone
+  (id, serial_number, model, state, weight_limit, battery_cap, version_no, created_at, updated_at, is_active)
+VALUES
+  (RANDOM_UUID(), RANDOM_UUID(), 'HEAVY_WEIGHT', 'IDLE', 500, 100, 0, NOW(), NOW(), TRUE);
